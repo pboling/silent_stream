3b0736407aa2108a8f74a82313b762e75cc5911a
Peter H. Boling <peter.boling@gmail.com>
2025-05-06 04:08:07 +0700

    ➕ require ruby_version

--- tests/test_silent_stream.rb
+++ tests/test_silent_stream.rb
@@ -6,6 +6,7 @@ require "minitest/reporters"
 require "minitest/autorun"
 require "mocha/minitest"
 require "ruby_engine"
+require "ruby_version"
 
 reporter_options = {color: true}
 Minitest::Reporters.use!([Minitest::Reporters::DefaultReporter.new(reporter_options)])
