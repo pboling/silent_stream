924baa49aad03eeeed2c34da2636840f066fe083
Peter H. Boling <peter.boling@gmail.com>
2025-05-06 04:13:41 +0700

    💚 Skip on JRuby 10.0

--- tests/test_silent_stream.rb
+++ tests/test_silent_stream.rb
@@ -112,7 +112,7 @@ class KernelTest < SilentStream::TestCase
   end
 
   def test_capture_stdout_system_call
-    skip("JRuby 9.2 doesn't capture here for some reason") if RubyEngine.jruby? && RubyVersion.is?("2.5")
+    skip("JRuby 9.2 & 10.0 do not capture here for some reason") if RubyEngine.jruby? && (RubyVersion.is?("2.5") || RubyVersion.is?("3.4"))
     assert_equal("STDOUT\n", MyClass.capture(:stdout) { system("echo STDOUT") })
   end
 end
